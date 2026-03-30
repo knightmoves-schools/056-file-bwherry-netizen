5ea8b6d9f97b9de3e657b6a3d16f0d952172d9ea
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-30 12:20:36 -0500

    Update FileManager.cs

--- FileManager.cs
+++ FileManager.cs
@@ -3,7 +3,7 @@
 public class FileManager{
 
     public void Create(string fileName){
-           fileName = Path.GetFileName();
+           string tempFileName = Path.GetTempFileName();
            string content = "hello file";
            File.WriteAllText(fileName, content);
     }
