bcd26d8b85eacb2f0d36ffd80a36973bdd81d9c1
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-30 12:23:01 -0500

    Refactor FileManager to remove temp file creation

--- FileManager.cs
+++ FileManager.cs
@@ -1,10 +1,10 @@
-﻿namespace knightmoves;
+namespace knightmoves;
 
-public class FileManager{
-
-    public void Create(string fileName){
-           string tempFileName = Path.GetTempFileName();
-           string content = "hello file";
-           File.WriteAllText(fileName, content);
+public class FileManager
+{
+    public void Create(string fileName)
+    {
+        string content = "hello file";
+        File.WriteAllText(fileName, content);
     }
 }
