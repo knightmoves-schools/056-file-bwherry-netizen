3f171b37ade7c00124dca1f261378e7de2eca185
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-30 12:26:53 -0500

    Refactor Create method in FileManager
    Simplified file creation by using the full file name.

--- FileManager.cs
+++ FileManager.cs
@@ -4,7 +4,6 @@ public class FileManager
 {
     public void Create(string fileName)
     {
-        var name = Path.GetFileName(fileName);
-        File.WriteAllText(name, "hello file");
+        File.WriteAllText(fileName, "hello file");
     }
 }
